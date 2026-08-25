42dba3cff32c0e14ef026ddf007949e744b55956
WilAzDev <jhonazcaratework@gmail.com>
2026-08-25 15:43:38 -0500

    feat: add logic for validation in the login form.

--- src/app/domains/auth/layout/layout.ts
+++ src/app/domains/auth/layout/layout.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CardModule } from 'primeng/card'
+import { ButtonModule } from 'primeng/button';
+import { TooltipModule } from 'primeng/tooltip';
 
 @Component({
   selector: 'app-layout',
   imports: [
     RouterOutlet,
-    CardModule
+    CardModule,
+    ButtonModule,
+    TooltipModule
   ],
   templateUrl: './layout.html',
   styleUrl: './layout.css',
--- src/app/domains/auth/pages/login/login.ts
+++ src/app/domains/auth/pages/login/login.ts
@@ -1,17 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { InputTextModule } from 'primeng/inputtext';
 import { IftaLabelModule } from 'primeng/iftalabel';
 import { ButtonModule } from 'primeng/button';
+import { LoginModel } from '@domains/auth/models/login';
+import { form, FormField, FormRoot, required, email } from '@angular/forms/signals';
 
 @Component({
   selector: 'app-login',
-  imports: [
-    InputTextModule,
-    IftaLabelModule,
-    ButtonModule
-  ],
+  imports: [InputTextModule, IftaLabelModule, ButtonModule, FormField, FormRoot],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
 export class Login {
+  loginModel = signal<LoginModel>({
+    email: '',
+    password: '',
+  });
+
+  loginForm = form(
+    this.loginModel,
+    (schemaPath) => {
+      required(schemaPath.email, { message: 'Please enter an email!' });
+      email(schemaPath.email,{ message: "Please enter a valid email!"})
+      required(schemaPath.password);
+    },
+    {
+      submission: {
+        action: async (formValue) => {
+          console.log(formValue().value());
+        },
+      },
+    },
+  );
 }
