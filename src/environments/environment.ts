2f3c3d77d4b650bc44e25530063fb4d17ddb4331
WilAzDev <jhonazcaratework@gmail.com>
2026-09-08 19:57:48 -0500

    fix: simplify redirect URLs in environment configuration

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,14 +4,8 @@ export const environment = {
     userPoolId: 'us-east-1_P4Y0dHQlO',
     userPoolClientId: '5crd6pl0am16u6h2kvlf2df5ru',
     cognitoDomain: 'us-east-1p4y0dhqlo.auth.us-east-1.amazoncognito.com',
-    redirectSignIn: [
-      'https://wilazdev-front.jhonazcaratework.workers.dev/',
-      'http://localhost:4200/'
-    ],
-    redirectSignOut: [
-      'https://wilazdev-front.jhonazcaratework.workers.dev/',
-      'http://localhost:4200/'
-    ],
-    scopes: ['openid', 'email', 'profile'],
+    redirectSignIn: 'https://wilazdev-front.jhonazcaratework.workers.dev/',
+    redirectSignOut: 'https://wilazdev-front.jhonazcaratework.workers.dev/',
+    scopes: ['openid', 'email', 'profile']
   }
 };
