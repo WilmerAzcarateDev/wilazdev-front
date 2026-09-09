f08261d65bb0eb27135845c765463a5cbd115202
WilAzDev <jhonazcaratework@gmail.com>
2026-09-08 20:07:59 -0500

    fix: update environment import to use the correct configuration

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
 import { Amplify } from 'aws-amplify';
-import { environment } from './environments/environment.development';
+import { environment } from './environments/environment';
 
 const { cognito } = environment;
 const {
